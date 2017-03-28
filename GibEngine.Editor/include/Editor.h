382f1b068bb051a06774bb7a9ed44bc23c5a90ce
Matt Ryder <matt@mattryder.co.uk>
2017-03-28 23:05:16 +0100

    Add the Editor & Test subdirectories, fix the CmakeLists for Engine

---
+++ GibEngine.Editor/include/Editor.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Game.h"
+
+namespace GibEditor
+{
+    //class Editor : public GibEngine::Game
+    //{
+    //public:
+    //    Editor();
+    //    ~Editor();
+
+    //    virtual void Render() override;
+    //    virtual void Update() override;
+    //};
+}
---
+++ GibEngine.Tests/src/Editor.cpp
@@ -0,0 +1,21 @@
+#include "..\include\Editor.h"
+
+//GibEditor::Editor::Editor() : GibEngine::Game("GibEditor")
+//{
+//}
+//
+//GibEditor::Editor::~Editor()
+//{
+//}
+//
+//void GibEditor::Editor::Render()
+//{
+//    Game::Render();
+//
+//    this->Update();
+//}
+//
+//void GibEditor::Editor::Update()
+//{
+//    Game::Update();
+//}
